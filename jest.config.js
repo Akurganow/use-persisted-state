aedfa94855335fe3236cd36c350f71dbe1c75069
Alex Kurganov <a.kurganow@gmail.com>
2019-09-04 21:54:30 +0300

    configs

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/**
+ * @type {jest.ProjectConfig}
+ */
+module.exports = {
+
+  roots: [
+    '<rootDir>/__tests__'
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'ts',
+    'tsx'
+  ]
+}
