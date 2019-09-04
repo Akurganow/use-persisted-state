97dca4f7e175aca083bb5f579fa9ced535bee7bc
Alex Kurganov <a.kurganow@gmail.com>
2019-09-04 22:23:20 +0300

    fix(examples): simplify

--- examples/local-storage/index.jsx
+++ examples/local-storage/index.jsx
@@ -3,24 +3,21 @@ import { render } from 'react-dom'
 
 import createPersisterState from '../../lib'
 
-const [usePersistedState, clear] = createPersisterState('simple_example', window.sessionStorage)
+const [usePersistedState, clear] = createPersisterState('simple_example')
 
 function App() {
-  const [count, setCount] = usePersistedState<number>('count', 0)
+  const [count, setCount] = usePersistedState('count', 0)
 
   return (
     <div>
       {count}{' '}
       <button
         onClick={() => {
-          setCount(prevCount => {
-            const newCount = prevCount + 1
-
-            return newCount
-          })
+          setCount(prevCount => prevCount + 1)
         }}>
         +
       </button>
+      <button onClick={() => { clear() }}>Clear</button>
     </div>
   )
 }
--- examples/session-storage/index.jsx
+++ examples/session-storage/index.jsx
@@ -3,25 +3,20 @@ import { render } from 'react-dom'
 
 import createPersisterState from '../../lib'
 
-const [usePersistedState, clear] = createPersisterState('simple_example')
+const [usePersistedState, clear] = createPersisterState('simple_example', window.sessionStorage)
 
 function App() {
-  const [count, setCount] = usePersistedState<number>('count', 0)
+  const [count, setCount] = usePersistedState('count', 0)
 
   return (
     <div>
       {count}{' '}
       <button
         onClick={() => {
-          setCount(prevCount => {
-            const newCount = prevCount + 1
-
-            return newCount
-          })
+          setCount(prevCount => prevCount + 1)
         }}>
         +
       </button>
-      <button onClick={() => { clear() }}>Clear</button>
     </div>
   )
 }
