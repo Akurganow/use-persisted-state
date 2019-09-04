48aefa65274e2bc75012cbf8c0fd192930db2386
Alex Kurganov <a.kurganow@gmail.com>
2019-09-04 21:55:02 +0300

    examples

---
+++ examples/session-storage/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import createPersisterState from '../../lib'
+
+const [usePersistedState, clear] = createPersisterState('simple_example', window.sessionStorage)
+
+export default function App() {
+  const [count, setCount] = usePersistedState<number>('count', 0)
+
+  return (
+    <div>
+      {count}{' '}
+      <button
+        onClick={() => {
+          setCount(prevCount => {
+            const newCount = prevCount + 1
+
+            return newCount
+          })
+        }}>
+        +
+      </button>
+    </div>
+  )
+}
+
+const root = document.getElementById('root')
+
+render(<App />, root)
