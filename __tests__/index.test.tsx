711d922ef23d02c513d1ba02e4bda31f5cc70499
Alexander Kurganov <me@akurganow.ru>
2020-06-02 12:41:58 +0300

    test: add initial value test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -25,7 +25,10 @@ describe('Integration Tests', () => {
       const [, setCount] = usePersistedState('count', initialValue)
 
       return (
-        <button onClick={() => setCount(prev => prev + 1)} data-testid={testButtonId}>
+        <button
+          onClick={() => setCount(prev => prev + 1)}
+          data-testid={testButtonId}
+        >
           Test Button
         </button>
       )
@@ -124,6 +127,63 @@ describe('Integration Tests', () => {
     })
 
     const testComponent = render(<Component />)
+
+    expect(testComponent.getByTestId(testComponentId).textContent).toBe(String(initialValue))
+  })
+
+  it('Should change state when set initial value', async () => {
+    const initialValue = 0
+    const testComponentId = 'test_count_component'
+    const testButtonId = 'test_count_button'
+    const testInitialButtonId = 'test_count_initial_button'
+
+    const Counter = () => {
+      const [count] = usePersistedState('count', initialValue)
+
+      return <span data-testid={testComponentId}>{count}</span>
+    }
+    const Button = () => {
+      const [, setCount] = usePersistedState('count', initialValue)
+
+      return (
+        <button
+          onClick={() => { setCount(initialValue + 1) }}
+          data-testid={testButtonId}
+        >
+          Test Button
+        </button>
+      )
+    }
+    const InitialButton = () => {
+      const [, setCount] = usePersistedState('count', initialValue)
+
+      return (
+        <button
+          onClick={() => { setCount(initialValue) }}
+          data-testid={testInitialButtonId}
+        >
+          Test Button
+        </button>
+      )
+    }
+
+    const testButton = render(<Button />)
+    const testInitialButton = render(<InitialButton />)
+    const testComponent = render(<Counter />)
+
+    expect(testComponent.getByTestId(testComponentId).textContent).toBe(String(initialValue))
+
+    act(() => {
+      fireEvent.click(testButton.getByTestId(testButtonId))
+    })
+
+    expect(testComponent.getByTestId(testComponentId).textContent).toBe(String(initialValue + 1))
+
+    act(() => {
+      fireEvent.click(testInitialButton.getByTestId(testInitialButtonId))
+    })
+
+    console.log(localStorage.__STORE__);
 
     expect(testComponent.getByTestId(testComponentId).textContent).toBe(String(initialValue))
   })
