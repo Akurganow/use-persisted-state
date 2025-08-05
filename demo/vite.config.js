e6f409e9a2105ffa6c881dcb2a32f196967c9edd
Alexander Kurganov <a.kurganow@gmail.com>
2025-08-05 16:55:26 +0200

    chore: update Vite alias resolution to use `fileURLToPath` and `URL` for compatibility

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
   root: 'demo',
   resolve: {
     alias: {
-      '@plq/use-persisted-state': resolve(__dirname, '../src'),
+      '@plq/use-persisted-state': resolve(fileURLToPath(new URL('.', import.meta.url)), '../src'),
     },
   },
   server: {
